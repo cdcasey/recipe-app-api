6bd72431c4709a7f7b30917ebbc071c2f97c2080
Chris Casey <cdcasey@gmail.com>
2019-05-24 09:30:22 -0500

    List tags endpoint

---
+++ app/recipe/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from recipe import views
+
+
+# Generate urls from view
+router = DefaultRouter()
+router.register('tags', views.TagViewSet)
+
+# Lookup for reverse function
+app_name = 'recipe'
+
+urlpatterns = [
+    path('', include(router.urls))  # all paths that match the recipe app
+]
